ef138fc6ad8ce2d62dcef1188beb8e3bd2cf7a68
bobrossrtx <73446766+bobrossrtx@users.noreply.github.com>
2021-04-08 14:08:45 +0100

    Updated Schema

--- models/note.js
+++ models/note.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 const marked = require('marked');
+// const slugify = require('slugify');
+const createDomPurifier = require('dompurify');
+const { JSDOM } = require('jsdom');
+const domPurify = createDomPurifier(new JSDOM().window);
 
 const noteSchema = new mongoose.Schema({
     title: {
@@ -16,8 +20,41 @@ const noteSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    /**
+     * Leaving slug out for now since
+     * I keep getting errors when redirect...
+     *      Errors:
+     *          Cannot read property '*' of null (* = all items in schema (e.g. title))
+     * 
+     * I get a result and everything shows up just fine,
+     * I get the error in my terminal
+     */
+
+    // slug: {
+    //     type: String,
+    //     required: true,
+    //     unique: true
+    // },
+    sanitizedHtml: {
+        type: String,
+        required: true
     }
 });
 
+noteSchema.pre('validate', function(next) {
+    // if (this.title && this.id) {
+    //     this.slug = slugify(this.title + " " + this.id, {
+    //         lower: true,
+    //         strict: true
+    //     });
+    // }
+
+    if (this.markdown) {
+        this.sanitizedHtml = domPurify.sanitize(marked(this.markdown));
+    } // Sanitizes the html from markdown to prevent malicious scripts put into the markdown
+
+    next();
+});
 
 module.exports = mongoose.model('Note', noteSchema);
